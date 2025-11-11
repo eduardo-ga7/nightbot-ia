f2f6fd9aa444a86313315676f61a366660caa670
eduardo-ga7 <jeduardonintendo@gmail.com>
2025-11-10 21:01:09 -0600

    Update package.js

--- package.js
+++ package.js
@@ -1,11 +1,8 @@
 {
-    "name": "nightbot-ia",
-    "version": "1.0.0",
-    "main": "index.js",
-    "type": "module",
-    "dependencies": {
-      "express": "^4.19.2",
-      "openai": "^4.73.0"
-    }
+  "type": "module",
+  "dependencies": {
+    "openai": "^4.0.0"
   }
+}
+
   
